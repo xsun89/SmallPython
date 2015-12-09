45469c82776f2331cfc7add7e4afac9372dcec42
xsun <sunxin89@gmail.com>
2015-12-08 16:10:59 -0800

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+#include "../include/object.h"
+#include "../include/intObject.h"
+#include "../include/engine.h"
+
+int main()
+{
+	ExcuteEngine engine;
+	engine.Excute();
+	return 0;
+}
+
